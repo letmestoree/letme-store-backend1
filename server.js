586d0e92653838465520128f3ad02ea23942757d
letmestoree <letme.store@letme.hub.pl>
2026-03-08 23:50:42 +0100

    Add nodemailer dependency to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Stripe = require("stripe");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
 
 const app = express();
 app.use(express.json());
